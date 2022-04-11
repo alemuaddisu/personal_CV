9ef8a3fc82ea17940cf285e43785574c5fd1da31
Addisu Damena Alemu <addisudamena49@gmail.com>
2022-04-10 21:31:33 -0400

    menu added

--- app/src/main/java/com/miu/aalemu/personal_cv/MainActivity.kt
+++ app/src/main/java/com/miu/aalemu/personal_cv/MainActivity.kt
@@ -3,6 +3,8 @@ package com.miu.aalemu.personal_cv
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
@@ -21,33 +23,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-//
-//        val adapter = MyAdapter(supportFragmentManager,lifecycle)
-//        viewpager.adapter = adapter
-//
-//
-//
-//
-//        TabLayoutMediator(tlayaout,viewpager){tab,position->
-//            when(position){
-//                0->{
-//                    tab.text="Home"
-//                    tab.setIcon(R.drawable.home)
-//                }
-//                1->{
-//                    tab.text="About Me"
-//                    tab.setIcon(R.drawable.about_me)
-//                }
-//                2->{
-//                tab.text="Work"
-//                tab.setIcon(R.drawable.work)
-//                }
-//                3->{
-//                    tab.text="Contact"
-//                    tab.setIcon(R.drawable.contact_me)
-//                }
-//            }
-//        }.attach()
+
+        val adapter = MyAdapter(supportFragmentManager,lifecycle)
+        viewpager.adapter = adapter
+
+
+
+
+        TabLayoutMediator(tlayaout,viewpager){tab,position->
+            when(position){
+                0->{
+                    tab.text="Home"
+                    tab.setIcon(R.drawable.home)
+                }
+                1->{
+                    tab.text="About Me"
+                    tab.setIcon(R.drawable.about_me)
+                }
+                2->{
+                tab.text="Work"
+                tab.setIcon(R.drawable.work)
+                }
+                3->{
+                    tab.text="Contact"
+                    tab.setIcon(R.drawable.contact_me)
+                }
+            }
+        }.attach()
 
 //        binding = ActivityMainBinding.inflate(layoutInflater)
 //        setContentView(binding.root)
@@ -68,5 +70,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.mainmenu,menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Toast.makeText(applicationContext,"$item menu selected",Toast.LENGTH_LONG).show()
+        return super.onOptionsItemSelected(item)
     }
 }
