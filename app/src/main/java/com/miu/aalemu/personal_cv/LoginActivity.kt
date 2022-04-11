9b06aba06845e65977b3ec84a20d182f459de91d
Addisu Damena Alemu <addisudamena49@gmail.com>
2022-04-11 18:59:14 -0400

    shared preference

--- app/src/main/java/com/miu/aalemu/personal_cv/LoginActivity.kt
+++ app/src/main/java/com/miu/aalemu/personal_cv/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.miu.aalemu.personal_cv
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -16,13 +17,14 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        createInitialUsers()
         sign_in.setOnClickListener{
+
+
+
             val inputUserName = editTextTextEmailAddress.text.toString().trim();
             val inputPassword = editTextTextPassword.text.toString().trim();
             if(inputUserName == "" || inputPassword == "")
-                goIn()
-//                alert("Please fill both user name and password")
+                alert("Please fill both user name and password")
             else {
                 if(authenticateUser(inputUserName, inputPassword)){
                     goIn()
@@ -36,8 +38,6 @@ class LoginActivity : AppCompatActivity() {
         var resultContract = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
                 result ->
             if(result.resultCode == Activity.RESULT_OK){
-                val new_user = result.data?.getSerializableExtra("user") as User
-                user.add(new_user)
                 alert("Registration completed !")
             }
             else
@@ -55,16 +55,16 @@ class LoginActivity : AppCompatActivity() {
             var input_userName = editTextTextEmailAddress.text.toString().trim();
 
             if (input_userName == "")
-                alert("Please enter the user name")
+                alert("Please enter the user namesssss")
             else {
 
-                var foundPassword = findUser(input_userName)?.password
+                var foundPassword = findPassword(input_userName)
                 if (foundPassword != null) {
 
 
                     val intent = Intent(Intent.ACTION_SEND)
                     intent.type = "text/plain"
-                    intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("addisudalemu@gmail.com"))
+                    intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(input_userName))
                     intent.putExtra(Intent.EXTRA_SUBJECT, "Your Username and Password")
                     intent.putExtra(
                         Intent.EXTRA_TEXT,
@@ -87,28 +87,24 @@ class LoginActivity : AppCompatActivity() {
     }
 
 
-    private fun createInitialUsers() {
-        user.add(User("Addisu","Alemu","addisu@gmail.com","pass123"))
-        user.add(User("Haailu","Geleta","haailu@gmail.com","pass345"))
-        user.add(User("Teshom","Girma","teshome@gmail.com","abc123"))
-        user.add(User("Sara","Shinika","sara@gmail.com","longpass234word"))
-    }
-
-
     private fun authenticateUser(inputUserName: String, inputPassword: String):Boolean{
-        for(currentUser in user){
-            if(currentUser.userName.equals(inputUserName) && currentUser.userName.equals(inputUserName))
-                return true;
-        }
+        val spf = getSharedPreferences("userList", Context.MODE_PRIVATE)
+        val password= spf.getString(inputUserName,"empty")
+        if(password.equals(inputPassword))
+            return true
+
         return false
     }
 
-    private fun findUser(inputUserName: String):User?{
-        for(currentUser in user){
-            if(currentUser.userName.equals(inputUserName))
-                return currentUser
-        }
-        return null;
+    private fun findPassword(inputUserName: String):String?{
+
+        val spf = getSharedPreferences("userList", Context.MODE_PRIVATE)
+        val password= spf.getString(inputUserName,"empty")
+        if(password.equals("empty"))
+            return null
+
+        return password
+
     }
 
     private fun goIn() {
